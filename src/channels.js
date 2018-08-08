50ef625873763f50e2842cce32ccdb5d3b8a2b46
Christopher Stoll <stollcri@gmail.com>
2018-08-07 20:44:10 -0400

    Publish events

--- src/channels.js
+++ src/channels.js
@@ -15,21 +15,21 @@ module.exports = function(app) {
     if(connection) {
       // Obtain the logged in user from the connection
       // const user = connection.user;
-      
+
       // The connection is no longer anonymous, remove it
       app.channel('anonymous').leave(connection);
 
       // Add it to the authenticated user channel
       app.channel('authenticated').join(connection);
 
-      // Channels can be named anything and joined on any condition 
-      
+      // Channels can be named anything and joined on any condition
+
       // E.g. to send real-time events only to admins use
       // if(user.isAdmin) { app.channel('admins').join(connection); }
 
       // If the user has joined e.g. chat rooms
       // if(Array.isArray(user.rooms)) user.rooms.forEach(room => app.channel(`rooms/${room.id}`).join(channel));
-      
+
       // Easily organize users by email and userid for things like messaging
       // app.channel(`emails/${user.email}`).join(channel);
       // app.channel(`userIds/$(user.id}`).join(channel);
@@ -44,13 +44,14 @@ module.exports = function(app) {
     console.log('Publishing all events to all authenticated users. See `channels.js` and https://docs.feathersjs.com/api/channels.html for more information.'); // eslint-disable-line
 
     // e.g. to publish all service events to all authenticated users use
-    return app.channel('authenticated');
+    // return app.channel('authenticated');
+    return app.channel('anonymous');
   });
 
   // Here you can also add service specific event publishers
   // e.g. the publish the `users` service `created` event to the `admins` channel
   // app.service('users').publish('created', () => app.channel('admins'));
-  
+
   // With the userid and email organization from above you can easily select involved users
   // app.service('messages').publish(() => {
   //   return [
@@ -58,4 +59,5 @@ module.exports = function(app) {
   //     app.channel(`emails/${data.recipientEmail}`)
   //   ];
   // });
+  // app.service('broadcasts').publish('patched', () => app.channel('anonymous'));
 };
--- src/mongodb.js
+++ src/mongodb.js
@@ -4,12 +4,12 @@ const MongoClient = require('mongodb').MongoClient;
 module.exports = function (app) {
   const config = app.get('mongodb');
   const dbName = url.parse(config).path.substring(1);
-  const promise = MongoClient.connect(config).then(client => {
+  const promise = MongoClient.connect(config, {useNewUrlParser: true}).then(client => {
     // For mongodb <= 2.2
     if(client.collection) {
       return client;
     }
-    
+
     return client.db(dbName);
   });
 
